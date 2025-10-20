c55dce2605d94960fbe1effbf5174f59b09aecaf
Mateus Torres <mateusmmt00@gmail.com>
2025-10-19 21:04:30 -0300

    feat: add Tooltip component

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     allowedHosts: true,
+  },
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname, "./src")
+    }
   },
   plugins: [react(), tailwindcss()],
 });
