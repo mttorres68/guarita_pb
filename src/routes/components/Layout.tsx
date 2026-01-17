57c27277c1d610d9f85c56ed72b7e932790e39b1
Mateus Torres <mateusmmt00@gmail.com>
2026-01-17 08:57:38 -0300

    feat: refactor Layout component to use LayoutHeader and AddMovementModal

--- src/routes/components/Layout.tsx
+++ src/routes/components/Layout.tsx
@@ -1,56 +1,37 @@
+import React, { useState } from "react";
 import { AppSidebar } from "@/components/app-sidebar";
-import { DateRangePicker } from "@/components/date-ranger";
 import { ThemeProvider } from "@/components/theme-provider";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet } from "react-router";
+import { useRouteInfo } from "@/hooks/useRouteInfo";
+import { AddMovementModal } from "@/components/AddMovementModal";
+import { LayoutHeader } from "@/components/LayoutHeader";
 
 const Layout: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const routeInfo = useRouteInfo();
+
   return (
     <>
       <ThemeProvider>
         <SidebarProvider>
           <AppSidebar />
           <SidebarInset>
-            <header className="sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator
-                orientation="vertical"
-                className="mr-2 data-[orientation=vertical]:h-4"
-              />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>
-                      <DateRangePicker />
-                    </BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-            </header>
+            <LayoutHeader
+              routeInfo={routeInfo}
+              onOpenModal={() => setIsModalOpen(true)}
+            />
             <div className="flex flex-1 flex-col gap-4 p-4">
               <Outlet />
-              {/* <div className="grid auto-rows-min gap-4 md:grid-cols-5">
-                {Array.from({ length: 20 }).map((_, i) => (
-                  <div
-                    key={i}
-                    className="bg-red-400 aspect-square rounded-xl"
-                  />
-                ))}
-              </div> */}
             </div>
           </SidebarInset>
         </SidebarProvider>
+
+        <AddMovementModal
+          open={isModalOpen}
+          onOpenChange={setIsModalOpen}
+          routeInfo={routeInfo}
+        />
       </ThemeProvider>
     </>
   );
